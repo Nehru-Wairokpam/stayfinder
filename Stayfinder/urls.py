81a03ef3af5f99778933883e613188e04cd185ba
Nehru <wairokpam26@gmail.com>
2024-10-17 08:16:12 +0530

    file handling and static

--- Stayfinder/urls.py
+++ Stayfinder/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from StayfinderApp import views
+from django.conf.urls.static import static
+from django.conf import settings
 
-urlpatterns = [
+urlpatterns = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
     path('admin/', admin.site.urls),
     path('', views.home),
 ]
