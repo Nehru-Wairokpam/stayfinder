368592864623189bb3ffc681bb178a1e4663d032
alankamei <gaipuilankmei@gmail.com>
2024-10-21 19:08:12 +0530

    Added Hotels button to list all available hotels [ hotels.html]

--- StayfinderApp/views.py
+++ StayfinderApp/views.py
@@ -8,11 +8,21 @@ def home(request):
     ads_list = AdsSlide.objects.all()
     picked_hotel_list = Hotel.objects.all().filter(picked_of_day=True)
     top_ten_room_list = Room.objects.all().filter(top_ten=True)
-
-
     context={
         "ads_list":ads_list,
         "picked_hotel_list":picked_hotel_list,
-        "top_ten_room_list":top_ten_room_list
+        "top_ten_room_list":top_ten_room_list,
     }
     return render(request, 'home.html',context)
+
+
+
+def hotels(request):
+    
+    hotel_lists = Hotel.objects.all()
+    context = {
+        "hotel_lists":hotel_lists
+    }
+    return render(request, 'hotels.html', context)
+
+
