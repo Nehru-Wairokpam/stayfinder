093073ab66fa824449f6d4c629e21d0deaa5bd6a
Nehru <wairokpam26@gmail.com>
2024-10-22 08:13:21 +0530

    hotel details

--- Stayfinder/urls.py
+++ Stayfinder/urls.py
@@ -23,5 +23,8 @@ from django.conf import settings
 urlpatterns = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
     path('admin/', admin.site.urls),
     path('', views.home),
+    path('hotels/', views.hotels),
+    path('hotel_details/', views.hotel_details),
+
 ]
 
